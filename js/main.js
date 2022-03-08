b3914cbd1fe873531e5b37a268cf5e36b1b0a11c
Ali Rabeea <aaa.rabeea1411@gmail.com>
2022-03-08 09:41:15 -0500

    implemented task adding and completion checkbox

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 let app = new Vue({
     el: "#vueApp",
     data: {
-        welcomeMessage: 'Hello World!'
+        title: 'To Do App',
+        lists: [],
+        newItem:'',
     },
     methods: {
+        addItem:function(){
+            let id = this.lists.length + 1;
+            if(this.newItem !== ''){
+                const newList = {id:id,item:this.newItem};
+                this.lists.push(newList);
+                this.newItem = '';
+            }
+        },
 
+        // removeItem:function(){
+            
+        // }
     },
     mounted() {
 
